bd885fa89a8ecda39a8001c581352488f57c7907
Andrew Pylypiv <pylypiv10@gmail.com>
2020-11-08 20:38:30 +0200

    first commit)

---
+++ main.py
@@ -0,0 +1,13 @@
+from waitress import serve
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/api/v1/hello-world-24')
+def index():
+    return "Hello world 24"
+
+
+if __name__ == '__main__':
+    serve(app)
